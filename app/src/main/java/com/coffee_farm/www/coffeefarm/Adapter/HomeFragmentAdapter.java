8746bc66851d4a7bfdd6a32065cb535eb57f8924
HanSangYeol <samh2006@naver.com>
2017-11-22 17:06:29 +0900

    Categorie별 Fragment수정중

---
+++ app/src/main/java/com/coffee_farm/www/coffeefarm/Adapter/HomeFragmentAdapter.java
@@ -0,0 +1,63 @@
+package com.coffee_farm.www.coffeefarm.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.coffee_farm.www.coffeefarm.Fragment.Home.AllViewFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.BeanFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.CoffeeMachineFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.EsprossoFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.GreenBeanFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.HandDripFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.PresentSetFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.SetProductFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.Home.TeebagFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.HomeFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.CategorieFragment;
+import com.coffee_farm.www.coffeefarm.Fragment.MyPageFragment;
+
+/**
+ * Created by the on 2017-11-22.
+ */
+
+public class HomeFragmentAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 9;
+
+    public HomeFragmentAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new AllViewFragment();
+            case 1:
+                return new GreenBeanFragment();
+            case 2:
+                return new BeanFragment();
+            case 3:
+                return new TeebagFragment();
+            case 4:
+                return new SetProductFragment();
+            case 5:
+                return new PresentSetFragment();
+            case 6:
+                return new EsprossoFragment();
+            case 7:
+                return new HandDripFragment();
+            case 8:
+                return new CoffeeMachineFragment();
+            default:
+                return null;
+        }
+    }
+}
