1b1f1d4ab65e7704671b919c7d1eddf92eb8eb18
HanSangYeol <samh2006@naver.com>
2017-11-22 10:09:52 +0900

    Initial commit

---
+++ app/src/main/java/com/coffee_farm/www/coffeefarm/Activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.coffee_farm.www.coffeefarm.Activity;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by the on 2017-11-22.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setValues();
+    public abstract void setupEvents();
+    public abstract void bindView();
+}
