75cc486183580fb078cfd392ce100459d721688c
HanSangYeol <samh2006@naver.com>
2017-11-23 15:02:13 +0900

    GirdView 삭제 Flowlayout 변경시도(더미데이터 삽입) Spinner 기능 생성(수정해야함)

--- app/src/main/java/com/coffee_farm/www/coffeefarm/Data/Product.java
+++
@@ -1,8 +0,0 @@
-package com.coffee_farm.www.coffeefarm.Data;
-
-/**
- * Created by the on 2017-11-23.
- */
-
-public class Product {
-}
--- app/src/main/java/com/coffee_farm/www/coffeefarm/Util/GlobalUtil.java
+++ app/src/main/java/com/coffee_farm/www/coffeefarm/Util/GlobalUtil.java
@@ -1,6 +1,6 @@
 package com.coffee_farm.www.coffeefarm.Util;
 
-import com.coffee_farm.www.coffeefarm.Data.Product;
+import com.coffee_farm.www.coffeefarm.Data.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,5 +11,26 @@ import java.util.List;
 
 public class GlobalUtil {
 
-    public static final List<Product> PRODUCT_LIST = new ArrayList<>();
+    public static final List<Bean> GRRENBEAN_LIST = new ArrayList<>();
+    public static final List<Bean> BEAN_LIST = new ArrayList<>();
+
+    public static void GREENBEAN_DATE(){
+        GRRENBEAN_LIST.clear();
+        GRRENBEAN_LIST.add(new Bean(1, "생두1", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        GRRENBEAN_LIST.add(new Bean(2, "생두2", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        GRRENBEAN_LIST.add(new Bean(3, "생두3", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        GRRENBEAN_LIST.add(new Bean(4, "생두4", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        GRRENBEAN_LIST.add(new Bean(5, "생두5", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        GRRENBEAN_LIST.add(new Bean(6, "생두6", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+    }
+
+    public static void BEAN_DATE(){
+        BEAN_LIST.clear();
+        BEAN_LIST.add(new Bean(1, "원두1", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        BEAN_LIST.add(new Bean(2, "원두2", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        BEAN_LIST.add(new Bean(3, "원두3", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        BEAN_LIST.add(new Bean(4, "원두4", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        BEAN_LIST.add(new Bean(5, "원두5", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+        BEAN_LIST.add(new Bean(6, "원두6", 10000, "제조사", 250,2500,1000,"등급","원산지", 100,true, null,"상세정보"));
+    }
 }
