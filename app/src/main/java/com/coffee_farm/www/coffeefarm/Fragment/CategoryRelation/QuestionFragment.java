239c74ca01110a048bf6b3804966bfd6bdc712dd
HanSangYeol <samh2006@naver.com>
2017-11-29 18:19:48 +0900

    문의게시판 Fragment에 2개의 bottomNavigationAC 구현예정 - 수정중

--- app/src/main/java/com/coffee_farm/www/coffeefarm/Fragment/CategoryRelation/QuestionFragment.java
+++ app/src/main/java/com/coffee_farm/www/coffeefarm/Fragment/CategoryRelation/QuestionFragment.java
@@ -10,8 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.coffee_farm.www.coffeefarm.QNAActivity;
-import com.coffee_farm.www.coffeefarm.QuestionActivity;
 import com.coffee_farm.www.coffeefarm.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
