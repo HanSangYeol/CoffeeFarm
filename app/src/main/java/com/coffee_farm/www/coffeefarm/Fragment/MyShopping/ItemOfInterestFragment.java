9b66d93991ff82c634ce1682760a6befb032a581
HanSangYeol <samh2006@naver.com>
2017-12-04 18:20:49 +0900

    20171204 마무리

--- app/src/main/java/com/coffee_farm/www/coffeefarm/Fragment/MyShopping/ItemOfInterestFragment.java
+++ app/src/main/java/com/coffee_farm/www/coffeefarm/Fragment/MyShopping/ItemOfInterestFragment.java
@@ -1,5 +1,6 @@
 package com.coffee_farm.www.coffeefarm.Fragment.MyShopping;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.coffee_farm.www.coffeefarm.R;
+import com.coffee_farm.www.coffeefarm.ShoppingBasketActivity;
 
 /**
  * Created by the on 2017-11-30.
@@ -17,11 +19,13 @@ import com.coffee_farm.www.coffeefarm.R;
 public class ItemOfInterestFragment extends Fragment {
 
     private android.widget.ImageView backBtn;
+    private ImageView cartBtn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item_of_interest, container, false);
+        this.cartBtn = (ImageView) view.findViewById(R.id.cartBtn);
         this.backBtn = (ImageView) view.findViewById(R.id.backBtn);
 
 
@@ -39,6 +43,13 @@ public class ItemOfInterestFragment extends Fragment {
     }
 
     private void setupEvents() {
+        cartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ShoppingBasketActivity.class);
+                startActivity(intent);
+            }
+        });
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
